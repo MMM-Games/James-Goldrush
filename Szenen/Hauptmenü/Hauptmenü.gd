b42ecbe09e3f648ec4a05768b9480c5d9c0d0375
Gamechanger99 <marcsimon2006@gmail.com>
2020-05-24 21:02:45 +0200

    Optionen Button hinzugefügt

--- Szenen/Hauptmenü/Hauptmenü.gd
+++ Szenen/Hauptmenü/Hauptmenü.gd
@@ -7,3 +7,12 @@ func _on_Spielen_pressed():
   yield($Button, "finished")
 # warning-ignore:return_value_discarded
   get_tree().change_scene("res://Szenen/Spiel/Spiel.tscn")
+
+
+func _on_Optionen_pressed():
+  $Button.stream = load("res://Szenen/Hauptmenü/Soundeffekte/Button.ogg")
+  $Button.play()
+  yield($Button, "finished")
+# warning-ignore:return_value_discarded
+  get_tree().change_scene("res://Szenen/Optionen/Optionen.tscn")
+
